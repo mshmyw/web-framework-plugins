a5f8135fb9b6b3e01e4a2e69cb6fb709acda749a
chenshaorui <617672431@qq.com>
2021-05-02 17:43:03 +0800

    chore(rollup-config-factory): use Rollup Babel and TypeScript plugins

--- tools/rollup-config-factory/src/index.js
+++ tools/rollup-config-factory/src/index.js
@@ -1,13 +1,30 @@
+const { babel } = require("@rollup/plugin-babel");
+const typescript = require("@rollup/plugin-typescript");
+
 const getRollupConfigurations = () => {
   return {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: [
       {
         file: "dist/index.js",
         format: "umd",
       },
     ],
+    plugins: [
+      getBabelPluginConfigurations(),
+      getTypeScriptPluginConfigurations(),
+    ],
   };
+};
+
+const getBabelPluginConfigurations = () => {
+  return babel({
+    babelHelpers: "runtime",
+  });
+};
+
+const getTypeScriptPluginConfigurations = () => {
+  return typescript();
 };
 
 module.exports = {
