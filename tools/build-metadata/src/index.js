6a634d72089ff833e78d5cfc07bcc73d08e25fce
chenshaorui <617672431@qq.com>
2021-05-11 22:56:43 +0800

    chore(build-metadata): add plugin build metadata

---
+++ tools/build-metadata/src/index.js
@@ -0,0 +1,5 @@
+const PLUGIN = require("./plugin.js");
+
+module.exports = {
+  PLUGIN,
+};
---
+++ tools/build-metadata/src/plugin.js
@@ -0,0 +1,4 @@
+module.exports = {
+  OUTPUT_BUNDLE_FILE_NAME: "index.js",
+  OUTPUT_DEFINITION_FILE_NAME: "plugin.json",
+};
