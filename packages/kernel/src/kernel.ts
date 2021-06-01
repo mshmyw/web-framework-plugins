34538e363b1be792e3633eb45f85325fbe788207
chenshaorui <617672431@qq.com>
2021-06-01 15:28:10 +0800

    feat(kernel): register component plugin

--- packages/kernel/src/kernel.ts
+++ packages/kernel/src/kernel.ts
@@ -1,3 +1,22 @@
-export class Kernel {}
+import { RuntimeData } from "./interfaces";
+
+export class Kernel {
+  private runtimeData: RuntimeData = {
+    componentPlugins: {},
+    components: {},
+  };
+
+  public registerComponentPlugin(name: string, components: string[]) {
+    this.runtimeData.componentPlugins[name] = {
+      name,
+    };
+
+    components.forEach((component) => {
+      this.runtimeData.components[component] = {
+        pluginName: name,
+      };
+    });
+  }
+}
 
 export const kernel: Kernel = new Kernel();
